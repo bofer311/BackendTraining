57aea2dd4a4ff6d00755453559075acf3ada6d7c
bofer311 <ferbogado59@gmail.com>
2024-05-31 19:54:23 -0300

    first commit

---
+++ SRC/app.js
@@ -0,0 +1,27 @@
+const http= require("http");
+
+let tasks=[];
+let Helloworld= "hola Mundo";
+//let idCounter=1;
+
+
+const server=http.createServer((req,res)=>{
+    const{method,url}=req;
+
+    if(url=== '/' && method === 'GET'){
+        res.statusCode=200;
+        res.setHeader('Content-Type','application/json');
+        res.end(JSON.stringify(Helloworld));
+    }
+    if(url=== '/tasks' && method === 'GET'){
+        res.statusCode=200;
+        res.setHeader('Content-Type','application/json');
+        res.end(JSON.stringify(tasks));
+    }
+
+});
+
+const port=3000;
+server.listen(port,()=>{
+    console.log(`Server running at http://localhost:${port}/`);
+});   
